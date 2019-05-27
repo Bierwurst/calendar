e312fc41f41a7198c85f25f431ce14fcea563edb
Bierwurst <35297707+Bierwurst@users.noreply.github.com>
2019-05-27 16:19:48 +0200

    first working version, added some styles, random website links and images

--- src/components/window.js
+++ src/components/window.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './components.css';
-import { directive } from '@babel/types';
+
 
 
 class Window extends Component{
@@ -9,36 +9,35 @@ class Window extends Component{
         this.state = {
             number: this.props.number,
             day: this.props.date.getDate(),
+            month: this.props.date.getMonth()+1,
             class: "btn btn-success b-size btn-bgr-number-",
-            toggle: this.props.toggle
+            toggle: this.props.toggle,
+            classToday: ""
         }
         this.handleClick = this.handleClick.bind(this);
 
     }
-    // componentDidMount(){
-        
-    //     (this.state.number < this.state.day && this.state.toggle) ? this.setState({class: "btn btn-bgr-number-"+this.state.number}) : this.setState({class: "btn btn-primary b-size btn-bgr-number-"})
-    // } 
-    // handleClick(e) {
-    //     // this.setState({
-    //     //     class: "btn-bgr-number-"+this.state.number
-    //     // })
-    //     // console.log(this.state.number, this.state.class)
-    //     console.log('hello')
-    //   }
 
 
 
       handleClick() {
-          (this.state.toggle && this.state.number <= this.state.day)  ? 
+
+        //!! IMPORTANT: this is the condition to make sure, only the content of the day and the days before is visible/available, so nobody can see the christmas pictures before its time
+        //this.state.toggle && this.state.number <= this.state.day && this.state.month ===12 
+        
+        //for development I changed it to this.state.toggle
+        
+        (this.state.toggle)  ? 
              this.setState({
                 class: "btn btn-bgr-number-"+this.state.number,
-                toggle: !this.state.toggle
+                toggle: !this.state.toggle,
+                classToday: "dontShow"
         }) 
         :    
             this.setState({
                     class: "btn btn-success b-size btn-bgr-number-",
-                    toggle: !this.state.toggle
+                    toggle: !this.state.toggle,
+                    classToday: ""
                 })
 
       }
@@ -47,28 +46,33 @@ class Window extends Component{
     render(){
         let number = this.props.number;
         return(
-            <div className="b-container">
-                <button className={this.state.class} date={this.props.date} onClick={this.handleClick}>{this.state.number}</button>
-                <div className="text-center link-style">
-                <a  href="https://www.youtube.com/watch?v=L1O0suGglg4">Day{this.state.number} Surprise</a>
-                </div>
-                
+            <div className="window-wrapper">
+
+                    {(this.state.number === this.state.day) 
+                    ? 
+                    <div className="b-container">
+                        <button className={this.state.class} date={this.props.date} onClick={this.handleClick}><p className={this.state.classToday}>today</p></button>
+                        <div className="text-center link-style">
+                        <a  href={this.props.surprise[number-1]} target="_blank" rel="noopener noreferrer">Day {this.state.number} Surprise</a>
+                        </div>
+                    </div>
+                    :
+                    (this.state.number < this.state.day)
+                    ?
+                    <div className="b-container">
+                        <button className={this.state.class} date={this.props.date} onClick={this.handleClick}><p className={this.state.classToday}>{this.state.number}</p></button>
+                        <div className="text-center link-style">
+                        <a  href={this.props.surprise[number-1]} target="_blank" rel="noopener noreferrer">Day{this.state.number} Surprise</a>
+                        </div>
+                    </div>
+                    :
+                    <div className="b-container">
+                    <button className={this.state.class} date={this.props.date} onClick={this.handleClick}><p className={this.state.classToday}>{this.state.number}</p></button>
+                    <div className="text-center link-style">
+                    <div>Day {this.state.number} Surprise</div>
+                    </div>
+                </div>}
             </div>
-            
-
-            // {console.log(this.state.number, this.state.day)}
-            // (this.state.number < this.state.day) 
-            // ? 
-            // <button className={`btn btn-bgr-number-${number}`} date={this.props.date} onClick={this.handleClick}></button>
-            // :
-            // <button className={this.state.class} date={this.props.date} onClick={this.handleClick}>{this.state.number}</button>
-
-
-
-
-
-
-
         )
     }
 }
